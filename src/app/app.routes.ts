44609282570e1181f8145332270cdc51e825ba1d
ventiladordewolframio <68878309+ventiladordewolframio@users.noreply.github.com>
2025-07-26 15:33:07 -0300

    not found component created routes adapted to include both home and not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { NotFound } from './notfound/notfound';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full',
+    },
+    {
+        path: 'home',
+        component: Home,
+    },
+    {
+        path: '**',
+        component: NotFound,
+    }
+
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Home } from "./home/home";
+////import { Home } from "./home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
