09431c77355b1c6058c2416abdf8b54b9a15392f
ventiladordewolframio <68878309+ventiladordewolframio@users.noreply.github.com>
2025-07-26 20:45:27 -0300

    todas as mudancas da versao anterior do projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full',
+    },
+    {
+        path: 'home',
+        component: HomeComponent,
+    },
+    {
+        path: '**',
+        component: NotFoundComponent,
+    }
+
+];
