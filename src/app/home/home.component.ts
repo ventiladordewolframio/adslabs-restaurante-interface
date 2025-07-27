aed09c7c7ab5d666a53026094d33cced59cca179
ventiladordewolframio <68878309+ventiladordewolframio@users.noreply.github.com>
2025-07-27 07:27:23 -0300

    single user request by id

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,33 +2,37 @@ import { Component } from '@angular/core';
 import { ApiService } from '../services/api.service';
 import { CommonModule } from '@angular/common';
 
+import { FormsModule } from '@angular/forms'; // For ngModel
+
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  standalone: true,  // Mark the component as standalone
+  imports: [CommonModule, FormsModule],  // Import CommonModule and FormsModule directly
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 
 export class HomeComponent {
-  nome = "123 teste";
+  clients: any[] = [];
 
-  clients: any[] = [];  // Array to store the clients' data
-  loading = false;  // Flag to track the loading state
+  client: any = null;
+  clientId: number = 1;
+
+  loading = false;
 
   constructor(private apiService: ApiService) { }
 
   // Method triggered when the button is clicked
-  onFetchData() {
+  fetchAllClientData() {
     this.loading = true;
     this.apiService.getAllClientData(true).subscribe(
       (response: any) => {
-        console.log('API Response:', response);  // Log the response to inspect the data
+        console.log('API Response:', response);
         if (response && response.dados && Array.isArray(response.dados)) {
-          // Assign the 'dados' array directly to clients
-          this.clients = response.dados;  // Directly pass the 'dados' array to clients
+          this.clients = response.dados;
         } else {
           console.warn('No valid client data found in response.');
-          this.clients = [];  // In case no data is available
+          this.clients = [];
         }
         console.log('API Response CLIENTS:', this.clients)
         this.loading = false;
@@ -38,5 +42,30 @@ export class HomeComponent {
         this.loading = false;
       }
     );
+  }
+
+  fetchSingleClientData(id: number) {
+    this.loading = true;
+    this.apiService.getSingleClientData(id).subscribe(
+      (response: any) => {
+        console.log('API Response:', response);
+        if (response) {
+          this.client = response.client;
+        } else {
+          console.warn('No valid client data found in response.');
+          this.client = [];
+        }
+        console.log('API Response CLIENT:', this.client)
+        this.loading = false;
+      },
+      (error) => {
+        console.error('Error fetching data:', error);
+        this.loading = false;
+      }
+    );
+  }
+
+  test(id: number) {
+    console.log(id)
   }
 }
