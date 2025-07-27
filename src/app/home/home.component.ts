fe34b396685566f94f6727874dd435259a06fdc3
ventiladordewolframio <68878309+ventiladordewolframio@users.noreply.github.com>
2025-07-27 13:53:14 -0300

    full api for client interface

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { ApiService } from '../services/api.service';
 import { CommonModule } from '@angular/common';
 
-import { FormsModule } from '@angular/forms'; // For ngModel
+import { FormsModule } from '@angular/forms';
+import { DatePipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,  // Mark the component as standalone
   imports: [CommonModule, FormsModule],  // Import CommonModule and FormsModule directly
+  providers: [DatePipe],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
@@ -26,6 +29,48 @@ export class HomeComponent {
   fetchAllClientData() {
     this.loading = true;
     this.apiService.getAllClientData(true).subscribe(
+      (response: any) => {
+        console.log('API Response:', response);
+        if (response && response.dados && Array.isArray(response.dados)) {
+          this.clients = response.dados;
+        } else {
+          console.warn('No valid client data found in response.');
+          this.clients = [];
+        }
+        console.log('API Response CLIENTS:', this.clients)
+        this.loading = false;
+      },
+      (error) => {
+        console.error('Error fetching data:', error);
+        this.loading = false;
+      }
+    );
+  }
+
+  fetchTop5ClientsByOrder() {
+    this.loading = true;
+    this.apiService.getTop5ClientByOrder().subscribe(
+      (response: any) => {
+        console.log('API Response:', response);
+        if (response && response.dados && Array.isArray(response.dados)) {
+          this.clients = response.dados;
+        } else {
+          console.warn('No valid client data found in response.');
+          this.clients = [];
+        }
+        console.log('API Response CLIENTS:', this.clients)
+        this.loading = false;
+      },
+      (error) => {
+        console.error('Error fetching data:', error);
+        this.loading = false;
+      }
+    );
+  }
+
+  fetchTop5ClientsByTotalSpent() {
+    this.loading = true;
+    this.apiService.getTop5ClientByTotalSpent().subscribe(
       (response: any) => {
         console.log('API Response:', response);
         if (response && response.dados && Array.isArray(response.dados)) {
@@ -63,6 +108,88 @@ export class HomeComponent {
         this.loading = false;
       }
     );
+  }
+
+  deleteSingleClientData(id: number) {
+    this.loading = true;
+    this.apiService.delSingleClientData(id).subscribe(
+      (response: any) => {
+        console.log('API Response:', response);
+        if (response) {
+          this.client = response.client;
+        } else {
+          console.warn('No valid client data found in response.');
+          this.client = [];
+        }
+        console.log('API Response CLIENT:', this.client)
+        this.loading = false;
+      },
+      (error) => {
+        console.error('Error fetching data:', error);
+        this.loading = false;
+      }
+    );
+  }
+
+  editSingleClientData(id: number) {
+    this.loading = true;
+
+    const newClientData = {
+      name: this.client.name,
+      cpf: this.client.cpf
+    };
+
+    this.apiService.putSingleClientData(id, newClientData).subscribe(
+      (response: any) => {
+        console.log('API Response:', response);
+        if (response && response.client) {
+          this.client = response.client;
+        } else {
+          console.warn('No valid client data found in response.');
+          this.client = [];
+        }
+        console.log('API Response CLIENT:', this.client);
+        this.loading = false;
+      },
+      (error) => {
+        console.error('Error fetching data:', error);
+        this.loading = false;
+      }
+    );
+  }
+
+  createSingleClientData() {
+    this.loading = true;
+
+    const newClientData = {
+      name: this.client.name,
+      cpf: this.client.cpf
+    };
+
+    this.apiService.postSingleClientData(newClientData).subscribe(
+      (response: any) => {
+        console.log('API Response:', response);
+        if (response && response.client) {
+          this.client = response.client;
+        } else {
+          console.warn('No valid client data found in response.');
+          this.client = [];
+        }
+        console.log('API Response CLIENT:', this.client);
+        this.loading = false;
+      },
+      (error) => {
+        console.error('Error fetching data:', error);
+        this.loading = false;
+      }
+    );
+  }
+
+  loadVariables(clientid: number, id: number, name: string, cpf: string) {
+    this.clientId = clientid
+    this.client.id = id
+    this.client.name = name
+    this.client.cpf = cpf
   }
 
   test(id: number) {
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -14,19 +14,31 @@ export class ApiService {
   // Method to get the data from the API
   getAllClientData(active: boolean): Observable<any> {
     const url = `${this.apiUrl}client?active=${active}`;
-    return this.http.get<any>(url);  // Send GET request to API
+    return this.http.get<any>(url);
   }
   getSingleClientData(id: number): Observable<any> {
     const url = `${this.apiUrl}client/${id}`;
-    return this.http.get<any>(url);  // Send GET request to API
+    return this.http.get<any>(url);
   }
-  getTop5ClientByOrder(id: number): Observable<any> {
+  getTop5ClientByOrder(): Observable<any> {
+    const url = `${this.apiUrl}client/top5orders?active=true`;
+    return this.http.get<any>(url);
+  }
+  getTop5ClientByTotalSpent(): Observable<any> {
+    const url = `${this.apiUrl}client/top5spent?active=true`;
+    return this.http.get<any>(url);
+  }
+  delSingleClientData(id: number): Observable<any> {
     const url = `${this.apiUrl}client/${id}`;
-    return this.http.get<any>(url);  // Send GET request to API
+    return this.http.delete<any>(url);
   }
-  getTop5ClientByTotalSpent(id: number): Observable<any> {
+  putSingleClientData(id: number, clientData: any): Observable<any> {
     const url = `${this.apiUrl}client/${id}`;
-    return this.http.get<any>(url);  // Send GET request to API
+    return this.http.put<any>(url, clientData);
   }
-  
+  postSingleClientData(clientData: any): Observable<any> {
+    const url = `${this.apiUrl}client`;
+    return this.http.post<any>(url, clientData);
+  }
+
 }
